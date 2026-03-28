8c3b7b9c7153996448ff17798850db0699208062
sathyajit-P <sudupa2005@gmail.com>
2026-03-28 19:02:07 +0530

    deployment config added

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://sathyajit-P.github.io',
+  base: '/Me',
   vite: {
     plugins: [tailwindcss()],
   },
